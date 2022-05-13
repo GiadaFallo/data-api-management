42bcc4d7208772169294c346d541ac599fcfd25f
GiadaFallo <giada.fallo@gmail.com>
2022-05-13 10:46:59 +0200

    retreive data function implemented

--- app/database.py
+++ app/database.py
@@ -1,8 +1,23 @@
-from dataclasses import asdict
-from app.config import settings
+from dataclasses import asdict, dataclass
+from typing import List
 from pymongo.mongo_client import MongoClient
 
+from app.config import settings
 from app.models.storage import CustomerDialog
+
+
+@dataclass
+class Result():
+    customer_id: int
+    dialog_id: int
+    text: str
+    language: str
+    timestamp: str
+
+
+def convert(doc):
+    return Result(doc['customer_id'], doc['dialog_id'], doc['data']['text'],
+                  doc['data']['language'], doc['data']['timestamp'].isoformat())
 
 
 class Database:
@@ -15,6 +30,23 @@ class Database:
     def store_data(self, customer_dialog: CustomerDialog):
         self.customer_dialogs_collection\
             .insert_one(asdict(customer_dialog))
+
+    def read_data(self, language=None, customer_id=None, skip=0, limit=10) -> List[Result]:
+
+        conditions = []
+        if language:
+            conditions.append({"data.language": language})
+
+        if customer_id:
+            conditions.append({'customer_id': customer_id})
+
+        final_condition = {'$and': conditions} if len(conditions) > 0 else {}
+
+        cursor = self.customer_dialogs_collection.aggregate(
+            [{"$unwind": "$data"}, {"$match": final_condition}, {"$sort": {"data.timestamp": -1}},
+             {"$skip": skip}, {"$limit": limit}])
+             
+        return [convert(doc) for doc in cursor]
 
 
 database = Database(settings.db_url, "chat", "customer_dialogs")
