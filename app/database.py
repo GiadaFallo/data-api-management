25041058f485139dc22784f0781b720216f6c4ca
GiadaFallo <giada.fallo@gmail.com>
2022-05-13 20:53:24 +0200

    database file formatted and conver function moved inside Result class

--- app/database.py
+++ app/database.py
@@ -7,21 +7,24 @@ from app.models.storage import CustomerDialog
 
 
 @dataclass
-class Result():
+class Result:
     customer_id: int
     dialog_id: int
     text: str
     language: str
     timestamp: str
 
-
-def convert(doc):
-    return Result(doc['customer_id'], doc['dialog_id'], doc['data']['text'],
-                  doc['data']['language'], doc['data']['timestamp'].isoformat())
-
+    @staticmethod
+    def from_db_document(doc):
+        return Result(
+            doc["customer_id"],
+            doc["dialog_id"],
+            doc["data"]["text"],
+            doc["data"]["language"],
+            doc["data"]["timestamp"].isoformat(),
+        )
 
 class Database:
-
     def __init__(self, db_url, db_name: str, collection_name: str):
         self.client = MongoClient(db_url)
         self.db = self.client[db_name]
@@ -30,20 +33,30 @@ class Database:
     def store_data(self, customer_dialog: CustomerDialog):
         self.customer_dialogs_collection.insert_one(asdict(customer_dialog))
 
-    def read_data(self, language=None, customer_id=None, skip=0, limit=10) -> List[Result]:
+    def read_data(
+        self, language=None, customer_id=None, skip=0, limit=10
+    ) -> List[Result]:
 
         conditions = []
         if language:
             conditions.append({"data.language": language})
 
         if customer_id:
-            conditions.append({'customer_id': customer_id})
+            conditions.append({"customer_id": customer_id})
 
-        final_condition = {'$and': conditions} if len(conditions) > 0 else {}
+        final_condition = {"$and": conditions} if len(conditions) > 0 else {}
 
         cursor = self.customer_dialogs_collection.aggregate(
-            [{"$unwind": "$data"}, {"$match": final_condition}, {"$sort": {"data.timestamp": -1}}, {"$skip": skip}, {"$limit": limit}])
+            [
+                {"$unwind": "$data"},
+                {"$match": final_condition},
+                {"$sort": {"data.timestamp": -1}},
+                {"$skip": skip},
+                {"$limit": limit},
+            ]
+        )
 
-        return list(map(convert, cursor))
+        return list(map(Result.from_db_document, cursor))
+
 
 database = Database(settings.db_url, "chat", "customer_dialogs")
